fb2aa56d79c69353fbe9064d6066dbade21134f0
Michael Kepple <kepplemr@gmail.com>
2013-04-15 21:07:32 -0400

    Initial commit

---
+++ DiskSim/Main.py
@@ -0,0 +1,14 @@
+'''
+Created on Apr 14, 2013
+
+@author: michael
+'''
+from SimPy.Simulation import *
+from SimPy.SimPlot import *
+from random import expovariate, seed
+
+class DiskRequests(Process):
+    """ Generator PEM that simulates disk requests """
+    def requests(self, numRequests, rate):
+        for i in range(numRequests):
+            
